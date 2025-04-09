e4f8d360e3e598936ef59f3ecb689a8aee7f89e2
TinyBoy407 <139179282+TinyBoy407@users.noreply.github.com>
2025-04-09 12:19:52 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -26,9 +26,8 @@ public class WordList
     for(int i = 0; i < myList.size(); i++){
       if(myList.get(i).length() == len){
         myList.remove(i);
-        i++;
+        i--;
       }
-  }
-    return;
+    }
   }
 }
